7ee9c00ff7a891d206b6dabf0d3a5861b4330f68
Krunal Patni <krunal.patni@cygabe.com>
2019-10-22 16:54:34 +0530

    Framework project check-in.

---
+++ FrameworkClassLibrary/FrameworkClassLibrary/Utility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkClassLibrary
+{
+    public class Utility
+    {
+        public static string GetProjectTypeName()
+        {
+            return Convert.ToString(System.Configuration.ConfigurationManager.AppSettings["name"]);
+        }
+    }
+}
